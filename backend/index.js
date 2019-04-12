189d185716c6418eb577b51c482afcf2aa119eb9
kaleido <kaleido@gmail.com>
2019-04-12 13:33:28 +0900

    new dragon class

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const Dragon =require("./dragon")
+
+
+const fooey=new Dragon(new Date(),"foory")
+const baloo=new Dragon(new Date(),"baloo")
+
+console.log("fooey",fooey)
+console.log("baloo",baloo)
